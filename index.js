f42e5f9fe1e1efc44cb72782aef6031f01f99eb9
Ahmet Avsar <ahmavsar1@outlook.com>
2023-06-22 12:20:55 +0300

    Verileri octokit ile Github'dan çekme

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const kelimeler = require('./kelimeler');
-const colors = require('colors')
-colors.enable()
-const didYouMean = require('didyoumean2').default;
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-const arrayFormat = kelimeler.split(`
-`).filter(item => item);
+;(async () => {
+  const { Octokit } = require('octokit')
+  require('colors').enable()
+  const didYouMean = require('didyoumean2').default
+  const readline = require('readline').createInterface({ input: process.stdin, output: process.stdout })
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
 
-readline.question('Bir kelimeyi yanlış yaz: ', word => {
-  const corrected = didYouMean(word, arrayFormat)
-  const capitalized = `${corrected.charAt(0).toUpperCase()}${corrected.slice(1)}`
-  console.log(`${capitalized.green} mi demek istedin?`);
-  readline.close();
-});
+  const b64DecodeUnicode = str => decodeURIComponent(atob(str).split('').map(char => '%' + ('00' + char.charCodeAt(0).toString(16)).slice(-2)).join(''))
 
+  const res = await octokit
+    .request('GET /repos/{owner}/{repo}/contents/{file_path}', {
+      owner: 'maidis',
+      repo: 'mythes-tr',
+      file_path: 'veriler/kelime-listesi.txt'
+    })
+  const kelimeler = b64DecodeUnicode(res.data.content).split(`
+`)
+  readline.question('Bir kelimeyi yanlış yaz: ', word => {
+    const corrected = didYouMean(word, kelimeler)
+    const capitalized = `${corrected.charAt(0).toUpperCase()}${corrected.slice(
+      1
+    )}`
+    console.log(`${capitalized.green} mi demek istedin?`)
+    readline.close()
+  })
+})()
